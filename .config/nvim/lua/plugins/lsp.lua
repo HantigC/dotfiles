09931bead81461c5f66e777e06a3e1f109ba2c4f
Stefan Cristian Hantig <stefan.hantig@telenav.com>
2022-05-23 17:05:24 +0300

    change python lsp, change theme

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -39,8 +39,8 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = {
-  'pyright',
-  -- 'pylsp',
+  -- 'pyright',
+  'pylsp',
   'dockerls',
   'rust_analyzer',
   'hls',
