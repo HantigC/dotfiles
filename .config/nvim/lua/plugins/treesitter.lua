52d91231c841550129000a68d034ec72d38fcfaf
HantigC <cristianhantig96@gmail.com>
2023-04-24 10:54:57 +0300

    add TS movements & folds

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,67 @@
 require("utils").safe_import(
   "nvim-treesitter.configs",
   function (treesitter)
+
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt.foldlevel = 0
+    vim.opt.foldlevelstart = 99
+
     treesitter.setup {
       autotag = {
         enable = true,
         filetypes = { "html" , "xml" },
       },
       textobjects = {
+        lsp_interop = {
+          enable = true,
+          border = 'none',
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ["<leader>df"] = "@function.outer",
+            ["<leader>dF"] = "@class.outer",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = { query = "@class.outer", desc = "Next class start" },
+            --
+            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+            ["]o"] = "@loop.*",
+            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+            --
+            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+            ["[o"] = "@loop.*",
+            ["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          goto_next = {
+            ["]c"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["[c"] = "@conditional.outer",
+          }
+        },
         select = {
           enable = true,
           lookahead = true,
@@ -42,7 +97,7 @@ require("utils").safe_import(
           -- * query_string: eg '@function.inner'
           -- * selection_mode: eg 'v'
           -- and should return true of false
-          -- include_surrounding_whitespace = true,
+          include_surrounding_whitespace = false,
         },
       },
       ensure_installed = {
@@ -51,15 +106,20 @@ require("utils").safe_import(
         "lua",
         "json",
         "python",
+        "dockerfile",
         "haskell",
         "rust",
         "html",
+        "yaml",
         "css",
+        "c",
+        "cmake",
         "cpp",
         "glsl",
         "make",
         "lua",
-        "query"
+        "markdown",
+        "query",
       },
       autopairs = {
         enable = true,
@@ -75,5 +135,15 @@ require("utils").safe_import(
         enable_autocmd = false,
       },
     }
+  end
+)
+
+require("utils").safe_import(
+ "nvim-treesitter-textobjects",
+  function (ts_repeat_move)
+    -- Repeat movement with ; and ,
+    -- ensure ; goes forward and , goes backward regardless of the last direction
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
   end
 )
