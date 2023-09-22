a702720ee0510573b326ae0d1afd6c59f1f4bc49
HantigC <cristianhantig96@gmail.com>
2023-09-22 18:32:27 +0300

    add mapping for telescope fuzzy word finding

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ require("utils").safe_import(
 
     map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', noremap)
     map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', noremap)
+    map('n', '<leader>fz', '<cmd>lua require"telescope.builtin".grep_string{ shorten_path = true, word_match = "-w", only_sort_text = true, search = "" }<CR>', noremap)
     map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', noremap)
     map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', noremap)
     map('n', '<leader>fc', '<cmd>Telescope command_history<cr>', noremap)
